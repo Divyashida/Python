a812135c7c3b17a761e86889958405ab826fb937
Divyashida <143320567+Divyashida@users.noreply.github.com>
2023-09-03 23:24:50 +0530

    Update datatype.py

--- datatype.py
+++ datatype.py
@@ -1,10 +1,10 @@
 
 #Question 4
 
-a,b,c,d=32,45.5,-10,"Bhagirath"
+a,b,c,d=32,45.5,-10,"divya"
 
 print(type(a))
 print(type(b))
 print(type(c))
 print(type(d))
-print(d,"Is Bhagirath is int ?",isinstance("Bhagirath",int))
+print(d,"Is divya is int ?",isinstance("divya",int))
